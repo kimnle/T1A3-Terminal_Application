dd17a25533ba916b58fd0a7467a85c6a74bde6b6
kimnle <kimnle002@gmail.com>
2023-12-16 11:10:05 +1100

    added hot/warm/cold/freezing conditional

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import random
 from pick import pick
 
 # Function: start screen
-def start():
+def start_screen():
     print("""
         _____    _     _    _____    _____    _____      ______    _    _    _____          _    _
        /  ___|  | |   | |  |  ___|  /  ___|  /  ___|    |__  __|  | |  | |  |  ___|      __| |__| |__
@@ -12,49 +12,58 @@ def start():
       |  \_| |  | \___/ |  | |___    ___|  |  ___|  |     |  |    | |  | |  | |___      |__   __   __|
        \_____|   \_____/   |_____|  |_____/  |_____/      |__|    |_|  |_|  |_____|        |_|  |_|
     """)
-    start.player_name = input("What's your name?\n")
-    start_options = ["Play", "Rules", "Scores", "Quit"]
-    start_option, index = pick(start_options)
+    start_screen.player_name = input("What's your name?\n")
+    start_screen_options = ["Play", "Rules", "Scores", "Quit"]
+    start_screen_option, index = pick(start_screen_options)
 
 # Function: player chooses difficulty after selecting play
-def difficulty():
-    difficulty_options = ["Easy", "Medium", "Hard"]
-    difficulty_option, index = pick(difficulty_options)
-    if difficulty_option == "Easy":
-        difficulty.max_range = 10
-        difficulty.attempts = 5
-    elif difficulty_option == "Medium":
-        difficulty.max_range = 100
-        difficulty.attempts = 10
+def difficulty_level():
+    difficulty_level_options = ["Easy", "Medium", "Hard"]
+    difficulty_level_option, index = pick(difficulty_level_options)
+    if difficulty_level_option == "Easy":
+        difficulty_level.max_range = 10
+        difficulty_level.attempts = 5
+    elif difficulty_level_option == "Medium":
+        difficulty_level.max_range = 100
+        difficulty_level.attempts = 10
     else:
-        difficulty.max_range = 1000
-        difficulty.attempts = 15
+        difficulty_level.max_range = 1000
+        difficulty_level.attempts = 15
 
 # Function: generate random number
 def random_number():
-    number = random.randint(1, difficulty.max_range)
+    random_number.number = random.randint(1, difficulty_level.max_range)
 
-# Function: get player's guess
-def guess():
-    for guess.n in range(difficulty.attempts):
+# Function: takes player's guess
+def take_guess():
+    for take_guess.n in range(difficulty_level.attempts):
         while True:
             try:
-                guess.player_guess = int(input(f"Guess a number between 1 and {difficulty.max_range}\n"))
-                if 1 <= guess.player_guess <= difficulty.max_range:
-                    return guess.player_guess
+                take_guess.player_guess = int(input(f"Guess a number between 1 and {difficulty_level.max_range}\n"))
+                if 1 <= take_guess.player_guess <= difficulty_level.max_range:
+                    return take_guess.player_guess
                 else:
                     print("Please enter a number between the specified range")
             except ValueError:
                 print("Please enter a number")
 
-# Function: checks player's guess
-def check_guess():
+# Function: verifies player's guess
+def verify_guess():
     while True:
-        if guess.player_guess < guess.n:
+        if take_guess.player_guess < take_guess.n:
             return "Your guess is too low"
-        elif guess.player_guess > guess.n:
+        elif take_guess.player_guess > take_guess.n:
             return "Your guess is too high"
         else:
             return "You guessed right!!"
             break
-
+        
+        # if take_guess.player_guess - random_number.number <= 10:
+        #     return "You're hot!!"
+        # elif take_guess.player_guess - random_number.number <= 20:
+        #     return "You're warm!"
+        # elif take_guess.player_guess - random_number.number <= 30:
+        #     return "You're cold"
+        # else:
+        #     return "You're freezing"
+        #     break
