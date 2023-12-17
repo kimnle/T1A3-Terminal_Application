19efd50b5f99f60c824e7236b43f8c34bcea1c68
kimnle <kimnle002@gmail.com>
2023-12-17 19:23:36 +1100

    trying new code in one file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#Import
-import game
 
-if __name__ == "__main__":
-    game.start_screen()
-    game.difficulty_level()
-    game.random_number()
-    game.take_guess()
-    game.verify_guess()
-    game.lets_play()
+
+# #Import
+# import game
+
+# if __name__ == "__main__":
+#     game.start_screen()
+#     game.difficulty_level()
+#     game.random_number()
+#     game.take_guess()
+#     game.verify_guess()
+#     game.lets_play()
