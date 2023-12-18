7d04ca6303a027cdda994f95a5bbc47847a007d4
kimnle <kimnle002@gmail.com>
2023-12-18 22:21:20 +1100

    added difficulty function in new code

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
+# Import
+import random
 
+# Function: player chooses difficulty
+def choose_difficulty():
+    print("Choose your difficulty")
+    print("Easy")
+    print("Medium")
+    print("Hard")
+
+    while True:
+        try:
+            difficulty = input("").lower()
+            if difficulty != "easy" or "medium" or "hard":
+                print("Please enter one of the three difficulties")
+                continue
+            break
+            except ValueError:
+                print("Please enter one of the three difficulties")
+
+    if difficulty == "easy":
+        attempts = 5
+    elif difficulty = "medium":
+        attempts = 10
+    else:
+        attempts = 15
+
+    return attempts
 
 # #Import
 # import game
